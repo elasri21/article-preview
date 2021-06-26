c4d2c4df044f6402cca4854ea428e1c5ffef6f2e
elasri21 <islame.edu@gmail.com>
2021-06-26 18:52:13 +0100

    files are added

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.btn-container button');
+const share = document.querySelector('.social');
+
+btn.addEventListener('click', function(){
+    share.style.display = 'block';
+})
